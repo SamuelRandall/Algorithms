ae7f7f052bcbb552e76f1c3be285b11e083f0d0f
samuelrandall <samuelrandall999@gmail.com>
2019-09-18 10:09:00 -0500

    added search file

--- dna_search.py
+++ dna_search.py
@@ -1,5 +1,11 @@
 from enum import IntEnum
 from typing import Tuple, List
+import time
+
+def time_method(method_to_run, *args):
+    start_time = time.time()
+    method_to_run(*args)
+    print(method_to_run.__name__, "Runtime: %s" % (time.time() - start_time))
 
 Nucleotide: IntEnum = IntEnum('Nucleotide', ('A', 'C', 'G', 'T'))
 Codon = Tuple[Nucleotide, Nucleotide, Nucleotide]
@@ -27,8 +33,26 @@ def linear_contains(gene: Gene, key_codon: Codon) -> bool:
 
 acg: Codon = (Nucleotide.A, Nucleotide.C, Nucleotide.G)
 gat: Codon = (Nucleotide.G, Nucleotide.A, Nucleotide.T)
-print(linear_contains(my_gene, acg))
-print(linear_contains(my_gene, gat))
+
+print(time_method(linear_contains, my_gene, acg))
+print(time_method(linear_contains, my_gene, gat))
 # due to the fact that these types have a contain method, this print statement will work without linear_contains
 print(acg in my_gene)
 print(gat in my_gene)
+
+def binary_contains(gene: Gene, key_codon: Codon) -> bool:
+    low: int = 0
+    high: int = len(gene) - 1
+    while low <= high:
+        mid: int = (low + high) // 2
+        if gene[mid] < key_codon:
+            low = mid + 1
+        elif gene[mid] > key_codon:
+            high = mid - 1
+        else:
+            return True
+    return False
+
+my_sorted_gene: Gene = sorted(my_gene)
+print(time_method(binary_contains, my_sorted_gene, acg))
+print(time_method(binary_contains, my_sorted_gene, gat))
